e3ef3d82c8eff27b6e7fc14c154572cd40426840
TOM <tuankc09071998@gmail.com>
2019-02-26 05:32:05 +0700

    Finish swt blackbox

---
+++ HNPower/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HNPower
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Game HANOI POWER!!!\n");
+
+            //int num = Int32.Parse(Console.ReadLine());
+            string num = "1";
+            ThapHN hn = new ThapHN(num);
+            Console.WriteLine("Node Path => " + hn.NodePath);
+            Console.WriteLine(hn.NumOfMove);
+            //Console.WriteLine(hn.StringOutput);
+            Console.ReadKey();
+        }
+    }
+
+    public class ThapHN
+    {
+        public int NumOfMove { get; set; }
+        public Boolean[] Node { get; set; }
+        public string NodeName { get; set; }
+        public string NodePath { get; set; }
+        public string StringOutput { get; set; }
+
+        public void Swap(int n, char x, char y)
+        {
+            //E
+            if (Node[4])
+            {
+                //After add E to Node [] => NodeName them after that don't add D into Node []
+                NodeName += "E";
+                Node[4] = false;
+            }
+            NodePath += "E";
+            NumOfMove++;
+            StringOutput += String.Format("Move disk {0} from {1} to {2}\n", n, x, y);
+        }
+        public void Recursive(int n, char a, char c, char b)
+        {
+            //After add D => NodeName them after that don't add D
+            if (Node[3])
+            {
+                Node[3] = false;
+                NodeName += "D";
+            }
+            NodePath += "D"; //every time call Recursive then check n =1 => Call D
+            //D
+            if (n == 1) Swap(1, a, c);
+            else
+            {
+                Recursive(n - 1, a, b, c);
+                Swap(n, a, c);
+                Recursive(n - 1, b, c, a);
+            }
+
+        }
+
+        public void MainProcess(int n)
+        {   //Node A is the first Node.
+            NodeName += "A";
+            NodePath += "A";
+            if (n < 1)//Node A
+            {
+                //Node B
+                NodeName += "B";
+                NodePath += "B";
+                StringOutput += "Error! Invalid input";
+            }
+            else Recursive(n, 'A', 'B', 'C');
+
+            //Node C
+            NodeName += "C";
+            NodePath += "C";
+        }
+        public ThapHN()
+        {
+
+        }
+
+        public ThapHN(string n)
+        {
+            try
+            {
+                int num = Int32.Parse(n);
+                if (num >= 15) throw new Exception();
+                StringOutput = "";
+                NodeName = "";
+                NumOfMove = 0;
+                NodePath = "";
+                Node = new Boolean[5];
+                // Init 5 node A,B,C,D,E
+                // If Program stop at a Node?
+                // => All Node after that node = false.
+                for (int i = 0; i < 5; i++)
+                {
+                    Node[i] = true;
+                }
+                MainProcess(num);
+                Console.WriteLine(StringOutput);
+            }
+            catch (Exception)
+            {
+                StringOutput += "Error! Invalid input";
+                //Console.WriteLine(StringOutput);        
+            }
+            
+        }
+    }
+}
---
+++ HNPowerTest/UnitTest1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HNPower;
+namespace HNPowerTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        /*
+        * Test case: 01
+        * Input: input = 0
+        * Expected result:"Error! Invalid input";
+        */
+        [TestMethod]
+        public void TC1()
+        {
+            string input = "0";
+            var output = "Error! Invalid input";
+            ThapHN hn = new ThapHN(input);
+            Assert.AreEqual(output, hn.StringOutput);
+        }
+
+        /*
+      * Test case: 02
+      * Input: input is positive integer (> 0)
+      * Expected result: num of steps to move disk
+      */
+
+        [TestMethod]
+        public void TC2()
+        {
+            string input = "4";
+            var output = 15;
+            ThapHN hn = new ThapHN(input);
+            Assert.AreEqual(output, hn.NumOfMove);
+        }
+            /*
+        * Test case: 03
+        * Input: input is negative integer ( < 0)
+        * Expected result: "Error! Invalid input";
+        */
+
+        [TestMethod]
+        public void TC3()
+        {
+            string input = "-4";
+            var output = "Error! Invalid input";
+            ThapHN hn = new ThapHN(input);
+            Assert.AreEqual(output, hn.StringOutput);
+        }
+
+        /*
+        * Test case: 04
+        * Input: input  = 1 
+        * Expected result: num of move  = 1;
+        */
+
+        [TestMethod]
+        public void TC4()
+        {
+            string input = "1";
+            var output = 1;
+            ThapHN hn = new ThapHN(input);
+            Assert.AreEqual(output, hn.NumOfMove);
+        }
+        /*
+        * Test case: 05
+        * Input: input  = 1 
+        * Expected result: Path throght Nodes: ADC;
+        */
+
+        [TestMethod]
+        public void TC5()
+        {
+            string input = "1";
+            var output = "ADEC";
+            ThapHN hn = new ThapHN(input);
+            Assert.AreEqual(output, hn.NodePath);
+        }
+
+        /*
+        * Test case: 06
+        * Input: input  < 1  
+        * Expected result: Path throght Nodes: ABC;
+        */
+
+        [TestMethod]
+        public void TC6()
+        {
+            string input = "-10";
+            var output = "ABC";
+            ThapHN hn = new ThapHN(input);
+            Assert.AreEqual(output, hn.NodePath);
+        }
+
+        /*
+       * Test case: 07
+       * Input: input in (0,1]
+       * Expected result: "Error! Invalid input";
+       */
+
+        [TestMethod]
+        public void TC7()
+        {
+            string input = "1.5";
+            var output = "Error! Invalid input";
+            ThapHN hn = new ThapHN(input);
+            Assert.AreEqual(output, hn.StringOutput);
+        }
+        /*
+       * Test case: 08
+       * Input: input integer in (1,15)
+       * Expected result: ;
+       */
+
+        [TestMethod]
+        public void TC8()
+        {
+            string input = "14";
+            var output = 16383;
+            ThapHN hn = new ThapHN(input);
+            Assert.AreEqual(output, hn.NumOfMove);
+        }
+
+        /*
+       * Test case: 09
+       * Input: input is character
+       * Expected result:"Error! Invalid input" ;
+       */
+
+        [TestMethod]
+        public void TC9()
+        {
+            string input = "abc";
+            var output = "Error! Invalid input";
+            ThapHN hn = new ThapHN(input);
+            Assert.AreEqual(output, hn.StringOutput);
+        }
+
+        /*
+     * Test case: 10
+     * Input: input = 15
+     * Expected result:"Error! Invalid input" ;
+     */
+
+        [TestMethod]
+        public void TC10()
+        {
+            string input = "15";
+            var output = "Error! Invalid input";
+            ThapHN hn = new ThapHN(input);
+            Assert.AreEqual(output, hn.StringOutput);
+        }
+
+        /*
+    * Test case: 11
+    * Input: input > 15
+    * Expected result:"Error! Invalid input" ;
+    */
+
+        [TestMethod]
+        public void TC11()
+        {
+            string input = "19";
+            var output = "Error! Invalid input";
+            ThapHN hn = new ThapHN(input);
+            Assert.AreEqual(output, hn.StringOutput);
+        }
+
+        /* 
+         * Test case: 12 - no loop
+         * Expected result: "ABC"
+         */
+        [TestMethod]
+        public void TC12()
+        {
+            string input = "0";
+            var output = "ABC";
+            ThapHN hn = new ThapHN(input);
+            Assert.AreEqual(output, hn.NodePath);
+        }
+
+        /* 
+         * Test case: 13 - 1 loop
+         * Expected result: "ADEC"
+         */
+        [TestMethod]
+        public void TC13()
+        {
+            string input = "1";
+            var output = "ADEC";
+            ThapHN hn = new ThapHN(input);
+            Assert.AreEqual(output, hn.NodePath);
+        }
+
+        /* 
+         * Test case: 14 - 2 loops
+         * Expected result: "ADDEEDEC"
+         */
+        [TestMethod]
+        public void TC14()
+        {
+            string input = "2";
+            var output = "ADDEEDEC";
+            ThapHN hn = new ThapHN(input);
+            Assert.AreEqual(output, hn.NodePath);
+        }
+
+        /* 
+         * Test case: 15 - more loops : 3 loops
+         * Expected result: "ADDDEEDEEDDEEDEC"
+         */
+        [TestMethod]
+        public void TC15()
+        {
+            string input = "3";
+            var output = "ADDDEEDEEDDEEDEC";
+            ThapHN hn = new ThapHN(input);
+            Assert.AreEqual(output, hn.NodePath);
+        }
+    }
+}
